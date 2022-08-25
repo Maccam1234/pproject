8b5377aea9ab3062dc0f48e5f27be7e14c553ed6
Maccam1234 <heng.cameron@gmail.com>
2022-08-24 20:54:19 -0700

    Added dotenv to secure database credentials

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+require('dotenv').config()
 const mongodb = require('mongodb');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const dbConn = mongodb.MongoClient.connect("mongodb+srv://superuser:superpassword@fusionpersonalproject.8jwti.mongodb.net/Project?retryWrites=true&w=majority");
+var username = process.env.DB_USERNAME
+var password = process.env.DB_PASSWORD
+var url = process.env.DB_URL
+
+const dbConn = mongodb.MongoClient.connect(`mongodb+srv://${username}:${password}@${url}`);
 
 app.use(express.static("public")); 
 
